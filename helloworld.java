d45d65e6fb799a98c9561d2b228dcb8b97df148e
eric <kemvoupatterson@gmail.com>
2020-12-12 14:45:54 -0800

    first version

---
+++ helloworld.java
@@ -0,0 +1,47 @@
+★★★Top Online Courses From ProgrammingKnowledge ★★★
+Python Programming Course ➡️ http://bit.ly/2vsuMaS ⚫️ http://bit.ly/2GOaeQB
+Java Programming Course ➡️ http://bit.ly/2GEfQMf ⚫️ http://bit.ly/2Vvjy4a
+Bash Shell Scripting Course ➡️ http://bit.ly/2DBVF0C ⚫️ http://bit.ly/2UM06vF
+Linux Command Line Tutorials ➡️ http://bit.ly/2IXuil0 ⚫️ http://bit.ly/2IXukt8
+C Programming Course ➡️ http://bit.ly/2GQCiD1 ⚫️ http://bit.ly/2ZGN6ej
+C++ Programming Course ➡️ http://bit.ly/2V4oEVJ ⚫️ http://bit.ly/2XMvqMs
+PHP Programming Course ➡️ http://bit.ly/2XP71WH ⚫️ http://bit.ly/2vs3od6
+Android Development Course ➡️ http://bit.ly/2UHih5H ⚫️ http://bit.ly/2IMhVci
+C# Programming Course ➡️ http://bit.ly/2Vr7HEl ⚫️ http://bit.ly/2W6RXTU
+JavaFx Programming Course ➡️ http://bit.ly/2XMvZWA ⚫️ http://bit.ly/2V2CoAi
+NodeJs Programming Course ➡️ http://bit.ly/2GPg7gA ⚫️ http://bit.ly/2GQYTQ2
+Jenkins Course For Developers and DevOps ➡️ http://bit.ly/2Wd4l4W ⚫️ http://bit.ly/2J1B1ug
+Scala Programming Tutorial Course ➡️ http://bit.ly/2PysyA4 ⚫️ http://bit.ly/2PCaVj2
+Bootstrap Responsive Web Design Tutorial ➡️ http://bit.ly/2DFQ2yC ⚫️ http://bit.ly/2VoJWwH
+MongoDB Tutorial Course ➡️ http://bit.ly/2LaCJfP ⚫️ http://bit.ly/2WaI7Ap
+QT C++ GUI Tutorial For Beginners ➡️ http://bit.ly/2vwqHSZ
+
+★★★ Online Courses to learn ★★★
+Get 2 FREE Months of Unlimited Classes from skillshare - https://skillshare.eqcm.net/r1KEj
+Data Science - http://bit.ly/2lD9h5L | http://bit.ly/2lI8wIl
+Machine Learning - http://bit.ly/2WGGQpb | http://bit.ly/2GghLXX  
+Artificial Intelligence - http://bit.ly/2lYqaYx | http://bit.ly/2NmaPya
+MERN Stack E-Degree Program - http://bit.ly/2kx2NFe | http://bit.ly/2lWj4no
+DevOps E-degree - http://bit.ly/2k1PwUQ | http://bit.ly/2k8Ypfy
+Data Analytics with R - http://bit.ly/2lBKqz8 | http://bit.ly/2lAjos3
+AWS Certification Training - http://bit.ly/2kmLtTu | http://bit.ly/2lAkQL1
+Projects in Java - http://bit.ly/2kzn25d | http://bit.ly/2lBMffs
+Machine Learning With TensorFlow - http://bit.ly/2m1z3AF | http://bit.ly/2lBMhnA
+Angular 8 - Complete Essential Guide - http://bit.ly/2lYvYRP
+Kotlin Android Development Masterclass - http://bit.ly/2GcblsI
+Learn iOS Programming Building Advance Projects - http://bit.ly/2kyX7ue
+
+
+★★★ Follow ★★★
+My Website - http://www.codebind.com
+
+DISCLAIMER: This video and description contains affiliate links, which means that if you click on one of the product links, I’ll receive a small commission. This help support the channel and allows us to continue to make videos like this. Thank you for the support!
+
+
+
+
+This video covers how to install git on Ubuntu 18.04, how to install git on Ubuntu Linux, how to use git on Ubuntu 18.04, setup git on windows
+Ubuntu git package, setup git server windows, Ubuntu git clone
+git-core, how to use GitHub Ubuntu , how to setup GitHub
+how to use GitHub Ubuntu , GitHub generate keys, git setup bash
+git install, git bash
